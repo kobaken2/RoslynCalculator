1467ee002a6e2130aa9e6574fdb63d1d2413629c
Yoichi Takeda <KFF00037@nifty.com>
2019-01-17 23:02:04 +0900

    プロジェクト ファイルを追加します。

---
+++ RoslynCalculator/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RoslynCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("変数");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("値");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("型");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("計算式");
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "変数";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.Location = new System.Drawing.Point(44, 66);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(100, 19);
+            this.labelMessage.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "計算式";
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(218, 65);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(164, 19);
+            this.textBoxValue.TabIndex = 3;
+            this.textBoxValue.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBoxValue_PreviewKeyDown);
+            // 
+            // listView
+            // 
+            this.listView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12});
+            this.listView.Location = new System.Drawing.Point(44, 122);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(468, 97);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 297);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.textBoxValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox labelMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ RoslynCalculator/Form1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoslynCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 計算式テキストボックスにてEnterが押下された時に、変数と計算式を元に各処理を行います
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxCalculate_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode != Keys.Return)
+            {
+                return;
+            }
+
+            if (this.textBoxValue.Text.StartsWith("clear(") || this.textBoxValue.Text.StartsWith("delete("))
+            {
+                // this に対するclaer()およびdelete()メソッドのスクリプト実行
+                CSharpScript.RunAsync(this.textBoxValue.Text, globals: this).Wait();
+                this.labelMessage.Text = String.Empty;
+                this.textBoxValue.Text = String.Empty;
+            }
+            else
+            {
+                // 単純な数値計算のスクリプト実行
+                MainAsync(this.textBoxValue.Text, this.textBoxValue.Text).Wait();
+                this.labelMessage.Text = String.Empty;
+                this.textBoxValue.Text = String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 単純な数値計算のスクリプト実行
+        /// </summary>
+        /// <param name="valueName">変数名</param>
+        /// <param name="method">計算式</param>
+        /// <returns>非同期操作オブジェクト</returns>
+        private async Task MainAsync(String valueName, String method)
+        {
+            String temp = String.Empty;
+
+            // 今まで計算した計算式をリスト化する
+            foreach (ListViewItem item in this.listView.Items)
+            {
+                temp += $"var {item.Text} = {item.SubItems[1].Text};";
+            }
+
+            // 今回の計算式を結合
+            temp += $" {method}";
+
+            var result = await CSharpScript.EvaluateAsync(temp);
+
+            this.listView.Items.Add(new ListViewItem(new String[] { valueName, result.ToString(), result.GetType().Name, method }));
+        }
+
+        /// <summary>
+        /// リストのクリア
+        /// </summary>
+        public void clear()
+        {
+            this.listView.Items.Clear();
+        }
+
+        /// <summary>
+        /// 指定した変数の削除
+        /// </summary>
+        /// <param name="valueName">削除する変数名</param>
+        public void delete(String valueName)
+        {
+            foreach (ListViewItem item in this.listView.Items)
+            {
+                if (item.Text == valueName)
+                {
+                    this.listView.Items.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        private void textBoxValue_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode != Keys.Return)
+            {
+                return;
+            }
+
+            if (this.textBoxValue.Text.StartsWith("clear(") || this.textBoxValue.Text.StartsWith("delete("))
+            {
+                // this に対するclaer()およびdelete()メソッドのスクリプト実行
+                CSharpScript.RunAsync(this.textBoxValue.Text, globals: this).Wait();
+                this.labelMessage.Text = String.Empty;
+                this.textBoxValue.Text = String.Empty;
+            }
+            else
+            {
+                // 単純な数値計算のスクリプト実行
+                MainAsync(this.textBoxValue.Text, this.textBoxValue.Text).Wait();
+                this.labelMessage.Text = String.Empty;
+                this.textBoxValue.Text = String.Empty;
+            }
+
+        }
+    }
+
+}
